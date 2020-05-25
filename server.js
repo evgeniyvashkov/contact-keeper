5e31d0a7b0aba8a23125e55043ccdfba930a9979
Eugene, Vashkov <zhekaprb1@gmail.com>
2020-05-25 23:22:10 +0300

    add connection to database

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
+const connectDb = require('./config/db');
 const app = express();
+
+//Database connect
+connectDb();
+
+//Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contacts', require('./routes/contacts'));
+app.use('/api/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 5000;
 
@@ -8,9 +16,5 @@ app.get('/', (req, res) => res.send({
     message: 'some message'
 }))
 
-//Define routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
-app.use('/api/auth', require('./routes/auth'));
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`))
