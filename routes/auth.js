e90005e2ed360bc1b343ff2797075f8a2e744fe2
Eugene, Vashkov <zhekaprb1@gmail.com>
2020-05-29 22:31:24 +0300

    add user authorization middleware

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,23 @@ const jwt = require('jsonwebtoken');
 const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
+const auth = require('../middleware/auth');
 
 //@route    get api/auth
 //@desc     get logged  in user
 //@access   Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user')
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+
+
+    res.send(req.user);
 });
 
 //@route    post api/auth
