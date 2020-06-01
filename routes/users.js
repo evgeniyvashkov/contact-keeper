7cc8bb86c25e12ace0d3f3d7406f308da7913f2d
Eugene, Vashkov <zhekaprb1@gmail.com>
2020-06-01 21:43:19 +0300

    fix eroor, when user exist

--- routes/users.js
+++ routes/users.js
@@ -24,7 +24,7 @@ router.post('/', [
     try {
         let user = await User.findOne({ email });
         if (user) {
-            return res.sendStatus(400).json({ message: 'User already exists' });
+            return res.status(400).json({ message: 'User already exists' });
         }
 
         const salt = await bcrypt.genSalt(10);
