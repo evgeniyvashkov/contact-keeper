584da06f8da485fe371d3057c04fd97ce386745d
Eugene, Vashkov <zhekaprb1@gmail.com>
2020-06-02 14:13:42 +0300

    Add client, Navbar component, Routes

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About page</h1>
+            <p className="my-1">This is full stack React app for contacts</p>
+            <p className="bg-dark p">1.0.0</p>
+        </div>
+    )
+}
